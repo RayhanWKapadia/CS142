d949afeb67baa171b70f9828e649cc35f317d5f3
Rayhan Kapadia <131191575+RayhanWKapadia@users.noreply.github.com>
2023-04-18 14:30:50 -0400

    Add files via upload

---
+++ Project 6/CS142_Project_6.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cstring>
+void reversedWord(char input[], int size);
+int main()
+{
+  std::cout<<"Welcome to Rayhan's Word Reversal Program! ";
+  int size;
+  //Declare character array of size 100
+  char word[100];
+  char answer = 'Y';
+
+    do 
+    {
+      std::cout<<"Please enter a word (100 Characters or Less): ";
+      std::cin>>word;
+      std::cout<<"Your word spelled backwards is: ";
+      //This is the function call
+      //strlen will find the length of a string and use it as word
+      reversedWord(word, strlen(word));
+	    std::cout<<word<<std::endl;
+      std::cout<<"Would you like to continue? ";
+      std::cin>>answer;
+    } 
+    while (answer != 'N'&& answer =='Y');
+  std::cout<<"Thank you! Goodbye!"<<std::endl;
+  return 0;
+}
+void reversedWord(char input[], int size)
+{
+  //This is the base case and will allow us to go through the array
+  if(size<2)
+  {
+	  return;
+  }
+  else
+  {
+    char i = input[0];
+    input[0] = input[size-1];
+    input[size-1] = i;
+   reversedWord(input+1, size-2);
+  }
+}
